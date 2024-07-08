24e40952f951b092e213ec660f20af3ec6ccea26
shwetapanchal09 <05panchalshweta@gmail.com>
2024-07-08 15:13:15 +0530

    PDF Generator

---
+++ src/main/java/com/redvinca/assignment/ecom_backend/controller/PdfGeneratorController.java
@@ -0,0 +1,39 @@
+package com.redvinca.assignment.ecom_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.redvinca.assignment.ecom_backend.serviceimpl.PdfGeneratorService;
+
+import java.io.IOException;
+
+@RestController
+public class PdfGeneratorController {
+
+    @Autowired
+    private PdfGeneratorService pdfService;
+
+    @GetMapping("/generate-pdf")
+    public ResponseEntity<byte[]> generatePdf() {
+        try {
+            byte[] pdfBytes = pdfService.generatePdf();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename=invoice.pdf");
+
+            return ResponseEntity
+            		.ok()
+                    .headers(headers)
+                    .contentType(org.springframework.http.MediaType.APPLICATION_PDF)
+                    .body(pdfBytes);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
