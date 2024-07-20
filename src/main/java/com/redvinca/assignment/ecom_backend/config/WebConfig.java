10c7a7f3061e9c5cce666498297a424d34c0c4f3
Praveen Singh <singhpraveenrajesh764@gmail.com>
2024-07-20 19:17:39 +0530

    add new email based registation feature

--- src/main/java/com/redvinca/assignment/ecom_backend/config/WebConfig.java
+++ src/main/java/com/redvinca/assignment/ecom_backend/config/WebConfig.java
@@ -7,15 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-	
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000") // Your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
